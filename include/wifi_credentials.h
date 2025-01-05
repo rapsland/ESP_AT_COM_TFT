a5502a3059873ac47f42d02384b416734b371634
Hüseyin Demir <rapsland@hotmail.com>
2025-01-05 10:57:18 +0100

    Update wifi_credentials.h

--- include/wifi_credentials.h
+++ include/wifi_credentials.h
@@ -1,4 +1,4 @@
 
   /*Put your SSID & Password*/
   const char* ssid = "FRITZ!Box 7590 XA";  // Enter SSID here
-  const char* password = "75670875703969771529";  //Enter Password here
+  const char* password = "xxxxxxxxxxxxxxxxxx";  //Enter Password here
